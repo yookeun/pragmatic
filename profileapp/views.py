d5d8aaaa696a56d6d633d1ee2da442feac7fc9f8
Yookeun <knoc720@gmail.com>
2023-08-15 09:43:02 +0900

    feat: 아티클 추가/목록/삭제 기능 추가

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-from django.urls import reverse_lazy, reverse
+from django.urls import  reverse
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, UpdateView
 
